5b51bb803076c914ea31591c5851319f459186d0
xarli <117117311+c4rli@users.noreply.github.com>
2023-02-05 01:57:32 +0000

    Fix cards loading when ajax call fails

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 function create5DayCard(dataArray, index) {
     var cardBody = $("<div>").addClass("card-body");
     console.log(moment(dataArray[0], "X").format("DD MM YYYY"));
-    var dataDate = $("<h5>").text(moment(dataArray[0], "X").format("ddd Do MMM YYYY"));
+    var dataDate = $("<h5>").text(moment(dataArray[0], "X").format("ddd Do MMM"));
     var dataTemp = $("<p>").html(`<b>Temperature:</b><br>${dataArray[1]}°c`);
     var dataWind = $("<p>").html(`<b>Wind Speed:</b><br> ${dataArray[2]}mph`);
     var dataHumidity = $("<p>").html(`<b>Humidity:</b><br> ${dataArray[3]}%`);
@@ -37,9 +37,11 @@ function generate5DayForecast(object) {
 }
 
 function generateTodaysForecast(object) {
+    console.log(object)
     var todayTemp = object.main.temp;
     var todayWind = object.wind.speed;
     var todayHumidity = object.main.humidity;
+    var bgImgURL = object.weather[0].icon;
     $("#todayCard").empty();
     $("#cityName").text(`${(object.name)}, ${(object.sys.country)}`); 
     
@@ -47,30 +49,33 @@ function generateTodaysForecast(object) {
     var dataTemp = $("<p>").html(`<b>Temperature:</b> ${todayTemp}°c`);
     var dataWind = $("<p>").html(`<b>Windspeed:</b> ${todayWind}mph`);
     var dataHumidity = $("<p>").html(`<b>Humidity:</b> ${todayHumidity}%`);
+    $("#todayCard").css("background-image",`url("http://openweathermap.org/img/wn/${bgImgURL}@4x.png")`)
 
     $("#todayCard").append(cardTitle, dataTemp, dataWind, dataHumidity);
 }
 
 $("#search-button").on("click", function (event) {
     event.preventDefault();
-    $(".searchContent").removeClass("hide");
-
+    
     var search = $("#search-input").val();
     var apiKey = "a32c896a5efe5c837799909bac3a9141";
     var queryURL1 = (`https://api.openweathermap.org/geo/1.0/direct?limit=5&q=${search}&appid=${apiKey}`);
-
+    
     $.ajax({
         url: queryURL1,
-        method: "GET"
-
+        method: "GET",
+        error: function(x){
+            
+        }
+        
     }).then(function (geoResponse) {
-
         var queryToday = (`https://api.openweathermap.org/data/2.5/weather?lat=${geoResponse[0].lat}&lon=${geoResponse[0].lon}&appid=${apiKey}&units=metric`);
         $.ajax({
             url: queryToday,
             method: "GET"
         }).then(function (response) {
             generateTodaysForecast(response);
+            $(".searchContent").removeClass("hide");
         });
 
         var query5Day = (`https://api.openweathermap.org/data/2.5/forecast?lat=${geoResponse[0].lat}&lon=${geoResponse[0].lon}&appid=${apiKey}&units=metric`);
@@ -79,8 +84,7 @@ $("#search-button").on("click", function (event) {
             method: "GET"
         }).then(function (response) {
             generate5DayForecast(response);
-        });
-                           
+        });              
         
 
     });
